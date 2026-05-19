33c41c0f082b48333d9f8a663d1645a2945dd2b2
Roxsteam <neves.aleex@gmail.com>
2026-05-18 21:11:51 -0300

    Extraindo o token que vem na requisição do usuário

--- auth/src/controllers/sessions-controller.ts
+++ auth/src/controllers/sessions-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { authConfig } from "@/configs/auth"
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
-  async create(request: Request, response: Response) {
-return response.json({message: "ok"})
+async create(request: Request, response: Response) {
+  const {username, password} = request.body
+
+  const fakeUser = {
+      id: 1,
+      username: "alex",
+      password: "123456", 
+    }
+
+    if (username !== fakeUser.username || password !== fakeUser.password){
+      throw new AppError ("Username e/ou senha incorreta", 401)
+    }
+
+    const {secret, expiresIn} = authConfig.jwt
+
+    const token = sign({}, secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+
+    })
+
+  return response.json({token})
 
   }
 }
---
+++ auth/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,22 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+
+export function ensureAuthenticated(
+request: Request, 
+response: Response, 
+next: NextFunction){
+
+    const authHeader = request.headers.authorization
+
+    if(!authHeader){
+        throw new AppError("JWT Token não informado")
+    }
+
+    const [,token]= authHeader.split(" ")
+
+    console.log(token)
+
+    return next()
+
+
+}
